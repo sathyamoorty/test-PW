39dc625a38a67cbf480b61377c9237dd7b3150e7
Sathyamoorty <sathyamoorthysathya409@gmail.com>
2026-03-31 15:53:27 +0530

    deleted file

--- tests/mytest.spec.ts
+++
@@ -1,7 +0,0 @@
-import { test, expect } from '@playwright/test';
-
-test("this is the first test", async ({ page }) => {
-   await page.goto("https://rbot.co.in");
-   await expect(page).toHaveTitle("Chatbot");
-   expect(page).toHaveURL("https://rbot.co.in");
-});
